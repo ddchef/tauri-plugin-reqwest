a078e49559eaed8e3b54b1fd81237c952fdb7085
ddchef <dev@xjie.me>
2022-10-29 00:30:57 +0800

    fix: 修复格式错误

--- webview-src/index.ts
+++ webview-src/index.ts
@@ -1,34 +1,34 @@
 import { invoke } from '@tauri-apps/api/tauri'
 
-export type ContentType ='application/json'|'multipart/form-data'|'application/x-www-form-urlencoded'|'text/html'|'text/plain'|'application/xml'
+export type ContentType = 'application/json' | 'multipart/form-data' | 'application/x-www-form-urlencoded' | 'text/html' | 'text/plain' | 'application/xml'
 
-export interface Options{
-  headers?:Record<string,string>[],
-  content_type?:ContentType,
-  basic_auth?:[string,string],
-  bearer_auth?:string,
-  query?:Record<string,string>[],
+export interface Options {
+  headers?: [string, string][],
+  content_type?: ContentType,
+  basic_auth?: [string, string],
+  bearer_auth?: string,
+  query?: [string, string][],
   timeout?: number,
   body?: string,
-  json?:any,
-  form?:Record<string,string>[]
+  json?: any,
+  form?: [string, string][]
 }
 
-export type Method ='Get'|'Post'|'Put'|'Patch'|'Delete'|'Head';
+export type Method = 'Get' | 'Post' | 'Put' | 'Patch' | 'Delete' | 'Head';
 
-export interface Response{
-  headers:Record<string,string>,
-  status:string,
+export interface Response {
+  headers: Record<string, string>,
+  status: string,
   content_type: ContentType,
-  body:string,
-  interval:number,
-  size:number
+  body: string,
+  interval: number,
+  size: number
 }
 
-export async function request(url:string,method?:Method,options?:Options):Promise<Response> {
-  return await invoke('plugin:reqwest|request',{
+export async function request(url: string, method?: Method, options?: Options): Promise<Response> {
+  return await invoke('plugin:reqwest|request', {
     url,
-    method:method||'Get',
-    options:options||{}
+    method: method || 'Get',
+    options: options || {}
   })
 }
